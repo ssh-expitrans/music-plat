bb2710944323b43629c0d3c1a8e1bea3135a0571
ssh-expitrans <sasha.expitrans@gmail.com>
2025-06-20 08:54:12 -0600

    fix link?

---
+++ src/app/demodashboard/studentdash/page.tsx
@@ -0,0 +1,6 @@
+// src/app/demodashboard/studentdash/page.tsx
+import DemoDashboard from '@/components/demodashboard';
+
+export default function DemoDashboardPage() {
+  return <DemoDashboard />;
+}
