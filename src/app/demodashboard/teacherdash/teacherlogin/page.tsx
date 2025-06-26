b14e09208c6e30bfc7c25d69597ef0c736f98ff1
ssh-expitrans <sasha.expitrans@gmail.com>
2025-06-26 09:12:17 -0600

    debug

--- src/app/demodashboard/teacherdash/teacherlogin/page.tsx
+++ src/app/demodashboard/teacherdash/teacherlogin/page.tsx
@@ -2,8 +2,14 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const TeacherLogin = dynamic(() => import("@/components/teacherlogin"), { ssr: false });
+
+console.log('TeacherLoginPage loaded');
+const TeacherLogin = dynamic(() => {
+  console.log('Dynamic import for TeacherLoginInner triggered');
+  return import("@/components/teacherlogin");
+}, { ssr: false });
 
 export default function TeacherLoginPage() {
+  console.log('TeacherLoginPage render function called');
   return <TeacherLogin />;
 }
